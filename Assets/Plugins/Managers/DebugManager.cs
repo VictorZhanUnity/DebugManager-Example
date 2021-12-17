3cff80007d31c3c60dc8b65e3c474f80f951f507
VictorZhanUnity <73773024+VictorZhanUnity@users.noreply.github.com>
2021-12-17 10:49:51 +0800

    修正註解

--- Assets/Plugins/Managers/DebugManager.cs
+++ Assets/Plugins/Managers/DebugManager.cs
@@ -6,7 +6,7 @@ namespace Managers.DebugHandler
 {
     /// <summary>
     /// 負責Debug.Log相關處理(用static架構設計)。
-    /// 若要引用，可直接在Script開頭使用"Debug = Managers.DebugHandler.DebugManager;"
+    /// 若要引用，可直接在Script開頭使用"Debug = Managers.DebugHandler.DebugManager;"來取代Debug關鍵字
     /// </summary>
     public class DebugManager
     {
